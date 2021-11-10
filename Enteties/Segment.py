3f299d4ec95c03c81c6888dd0b452919727d5ae1
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-11-10 02:43:11 +0200

    added Segment entity

---
+++ Enteties/Segment.py
@@ -0,0 +1,7 @@
+class Segment:
+    def __init__(self, start, duration, loudness_start, loudness_max_time, loudness_max):
+        self.start = start
+        self.duration = duration
+        self.loudness_start = loudness_start
+        self.loudness_max_time = loudness_max_time
+        self.loudness_max = loudness_max
