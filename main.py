4a59add5ec430287f834881e8f39b50f1a7586c5
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-11-10 11:27:11 +0200

    added simple ETL from PDL to PDWH

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+
+from Enteties.MetaInformation import MetaInformation
+from Enteties.Section import Section
+from Enteties.Segment import Segment
+from Enteties.Track import Track
+
+
+def main():
+    resource = open("Resources/classical.json", "r", encoding="utf-8")
+    pseudo_data_lake = json.load(resource)
+    pseudo_data_warehouse = [Track(key,
+                                   pseudo_data_lake[key]["artist"],
+                                   pseudo_data_lake[key]["song"],
+                                   MetaInformation(pseudo_data_lake[key]["meta"]["track"]["duration"],
+                                                   [Section(section["start"],
+                                                            section["duration"],
+                                                            section["loudness"])
+                                                    for section in pseudo_data_lake[key]["meta"]["sections"]],
+                                                   [Segment(segment["start"],
+                                                            segment["duration"],
+                                                            segment["loudness_start"],
+                                                            segment["loudness_max_time"],
+                                                            segment["loudness_max"]
+                                                            )
+                                                    for segment in pseudo_data_lake[key]["meta"]["segments"]]
+                                                   )
+                                   )
+                             for key in pseudo_data_lake.keys()]
+
+
+if __name__ == '__main__':
+    main()
