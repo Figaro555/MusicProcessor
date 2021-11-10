18229a93c1bbae27a1d025e402872355f6f00dfb
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-11-10 02:42:11 +0200

    added Track entity

---
+++ Enteties/Track.py
@@ -0,0 +1,10 @@
+class Track:
+    """Class that describes each line in file"""
+
+    def __init__(self, identical_num, artist, song_name, meta_information):
+        self.identical_num = identical_num
+        self.artist = artist
+        self.song_name = song_name
+        self.meta_information = meta_information
+
+
