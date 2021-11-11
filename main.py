460c6ec8feb51dec780d1d373b0f5e44ad575b34
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-11-11 16:47:22 +0200

    added insertion in db

--- main.py
+++ main.py
@@ -44,8 +44,25 @@ def main():
 
         )
 
-        cursor = connection.cursor()
-        print(connection.commit())
+        for track in local_pseudo_data_warehouse:
+            with connection.cursor() as cursor:
+                track_insert_queue = """INSERT INTO Tracks(author, song, duration)  VALUES (%s, %s, %s) RETURNING id"""
+                cursor.execute(track_insert_queue, (track.artist, track.song_name, track.duration))
+                track_id = cursor.fetchone()[0]
+
+                for segment in track.segments:
+                    segment_insert_queue = """INSERT INTO Segment(start, duration, loudness_start, loudness_max_time, loudness_max, track_id)  VALUES (%s, %s, %s, %s, %s, %s)"""
+                    cursor.execute(segment_insert_queue, (
+                        segment.start, segment.duration, segment.loudness_start, segment.loudness_max_time,
+                        segment.loudness_max, track_id))
+
+                for section in track.sections:
+                    section_insert_queue = """INSERT INTO Section(start, duration, loudness, track_id)  VALUES (%s, %s, %s, %s)"""
+                    cursor.execute(section_insert_queue, (
+                        section.start, section.duration, section.loudness, track_id))
+
+                print(track_id)
+            print(track.song_name)
 
     except Exception as _ex:
         print("[INFO] Error while working with PostgreSQL", _ex)
