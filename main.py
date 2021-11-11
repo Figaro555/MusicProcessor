e3aee0e8257e1b707aed5c7008d1d3e6eae3801a
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-11-11 15:55:48 +0200

    returned generation of local data DWH

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-from PseudoDataLake import PseudoDataLake
-
-
-from config import host, user, password, db_name, port
 import psycopg2
+
+from Enteties.Section import Section
+from Enteties.Segment import Segment
+from Enteties.Track import Track
+from PseudoDataLake import PseudoDataLake
+from config import host, user, password, db_name, port
 
 
 def main():
     pseudo_data_lake = PseudoDataLake()
+    local_pseudo_data_warehouse = []
+
+    for category in pseudo_data_lake.json_map:
+        local_pseudo_data_warehouse += ([Track(key,
+                                               pseudo_data_lake.json_map[category][key]["artist"],
+                                               pseudo_data_lake.json_map[category][key]["song"],
+                                               pseudo_data_lake.json_map[category][key]["meta"]["track"][
+                                                   "duration"],
+                                               [Section(section["start"],
+                                                        section["duration"],
+                                                        section["loudness"])
+                                                for section in
+                                                pseudo_data_lake.json_map[category][key]["meta"]["sections"]],
+                                               [Segment(segment["start"],
+                                                        segment["duration"],
+                                                        segment["loudness_start"],
+                                                        segment["loudness_max_time"],
+                                                        segment["loudness_max"]
+                                                        )
+                                                for segment in
+                                                pseudo_data_lake.json_map[category][key]["meta"]["segments"]]
+
+                                               )
+                                         for key in pseudo_data_lake.json_map[category].keys()])
 
     try:
         connection = psycopg2.connect(
@@ -17,6 +43,7 @@ def main():
             port=port
 
         )
+
         cursor = connection.cursor()
         print(connection.commit())
 
