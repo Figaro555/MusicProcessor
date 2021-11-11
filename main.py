3558b4c331a34f862f67dca346976e205cee6628
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-11-11 15:17:26 +0200

    added db initialization

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-
 from PseudoDataLake import PseudoDataLake
+
+
+from config import host, user, password, db_name, port
+import psycopg2
 
 
 def main():
     pseudo_data_lake = PseudoDataLake()
 
+    try:
+        connection = psycopg2.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=db_name,
+            port=port
 
+        )
+        cursor = connection.cursor()
+        print(connection.commit())
+
+    except Exception as _ex:
+        print("[INFO] Error while working with PostgreSQL", _ex)
+    finally:
+        if connection:
+            connection.close()
 
 
 if __name__ == '__main__':
