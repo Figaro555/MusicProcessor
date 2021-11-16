10d9d0ba08fa7cfe616549e4d1a0ba1d12486db1
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-11-16 02:10:25 +0200

    moved DB logic into different class

---
+++ DBLoader.py
@@ -0,0 +1,51 @@
+import psycopg2
+from Configs.config import host, user, password, db_name, port
+
+
+class DBLoader:
+
+    def __init__(self, local_dwh):
+        self.local_DWH = local_dwh
+
+    def load_data(self):
+        try:
+            connection = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=db_name,
+                port=port
+
+            )
+
+            for track in self.local_DWH:
+                with connection.cursor() as cursor:
+                    track_insert_queue = """    INSERT INTO
+                                                Tracks(author, song, duration)  
+                                                VALUES (%s, %s, %s) RETURNING id"""
+                    cursor.execute(track_insert_queue, (track.artist, track.song_name, track.duration))
+                    track_id = cursor.fetchone()[0]
+
+                    for segment in track.segments:
+                        segment_insert_queue = """  INSERT INTO 
+                                                    Segment(start, duration, loudness_start, loudness_max_time, loudness_max, track_id)  
+                                                    VALUES (%s, %s, %s, %s, %s, %s)"""
+                        cursor.execute(segment_insert_queue, (
+                            segment.start, segment.duration, segment.loudness_start, segment.loudness_max_time,
+                            segment.loudness_max, track_id))
+
+                    for section in track.sections:
+                        section_insert_queue = """  INSERT INTO 
+                                                    Section(start, duration, loudness, track_id)
+                                                    VALUES (%s, %s, %s, %s)"""
+                        cursor.execute(section_insert_queue, (
+                            section.start, section.duration, section.loudness, track_id))
+
+                    print(track_id)
+                print(track.song_name)
+
+        except Exception as _ex:
+            print("[INFO] Error while working with PostgreSQL", _ex)
+        finally:
+            if connection:
+                connection.close()
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import psycopg2
-
+from DBLoader import DBLoader
 from Enteties.Section import Section
 from Enteties.Segment import Segment
 from Enteties.Track import Track
 from PseudoDataLake import PseudoDataLake
-from Configs.config import host, user, password, db_name, port
 
 
 def main():
@@ -33,42 +31,8 @@ def main():
 
                                                )
                                          for key in pseudo_data_lake.json_map[category].keys()])
-
-    try:
-        connection = psycopg2.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=db_name,
-            port=port
-
-        )
-
-        for track in local_pseudo_data_warehouse:
-            with connection.cursor() as cursor:
-                track_insert_queue = """INSERT INTO Tracks(author, song, duration)  VALUES (%s, %s, %s) RETURNING id"""
-                cursor.execute(track_insert_queue, (track.artist, track.song_name, track.duration))
-                track_id = cursor.fetchone()[0]
-
-                for segment in track.segments:
-                    segment_insert_queue = """INSERT INTO Segment(start, duration, loudness_start, loudness_max_time, loudness_max, track_id)  VALUES (%s, %s, %s, %s, %s, %s)"""
-                    cursor.execute(segment_insert_queue, (
-                        segment.start, segment.duration, segment.loudness_start, segment.loudness_max_time,
-                        segment.loudness_max, track_id))
-
-                for section in track.sections:
-                    section_insert_queue = """INSERT INTO Section(start, duration, loudness, track_id)  VALUES (%s, %s, %s, %s)"""
-                    cursor.execute(section_insert_queue, (
-                        section.start, section.duration, section.loudness, track_id))
-
-                print(track_id)
-            print(track.song_name)
-
-    except Exception as _ex:
-        print("[INFO] Error while working with PostgreSQL", _ex)
-    finally:
-        if connection:
-            connection.close()
+        db_loader = DBLoader(local_pseudo_data_warehouse)
+        db_loader.load_data()
 
 
 if __name__ == '__main__':
