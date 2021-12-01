012c24682f8b71034655ccb014f0fc0f61185829
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-12-02 00:16:36 +0200

    added test with comparing result fully to TestOnData

--- test/TransformersTest/Kaggle/TestsOnDataTransFormetTest.py
+++ test/TransformersTest/Kaggle/TestsOnDataTransFormetTest.py
@@ -49,6 +49,47 @@ class KaggleTestsOnDataTransformerTest(TestCase):
         result = self.tt.transform_to_local_array(self.parsed_json)
         self.assertTrue(type(result[0].sections[0]) is Section)
 
+    def test_transform_to_local_array_comparing_full_result(self):
+        result = self.tt.transform_to_local_array(self.parsed_json)
+        expected_result = Track(
+            identical_num='spotify:track:5gICNh1EHmDlpyWERFitXG',
+            artist='Southern Avenue',
+            song_name='Savior',
+            duration=223.49048,
+            sections=[Section(duration=36.15176,
+                              loudness=-8.416,
+                              start=0.0
+                              ),
+                      Section(duration=42.0297,
+                              loudness=-3.606,
+                              start=36.15176
+                              )],
+            segments=[Segment(duration=0.12163,
+                              loudness_max=-60.0,
+                              loudness_max_time=0.0,
+                              loudness_start=-60.0,
+                              start=0.0
+                              ),
+                      Segment(duration=0.55129,
+                              loudness_max=-9.87,
+                              loudness_max_time=0.25127,
+                              loudness_start=-60.0,
+                              start=0.12163
+                              ),
+                      Segment(duration=0.39628,
+                              loudness_max=-18.931,
+                              loudness_max_time=0.02625,
+                              loudness_start=-23.007,
+                              start=0.67293
+                              )
+                      ]
+        )
+
+        self.assertTrue(
+            result[0],
+            expected_result
+        )
+
 
 if __name__ == '__main__':
     main()
