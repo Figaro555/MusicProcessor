5cf4d796e64291fb957f1a4ed798f1a15a209085
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-12-01 23:57:35 +0200

    imlpemented equals method to YT Entities

--- project/Entities/YouTubeData/Video.py
+++ project/Entities/YouTubeData/Video.py
@@ -1,4 +1,13 @@
 class Video:
+    def __eq__(self, o: object) -> bool:
+        if isinstance(o, Video):
+            return (self.video_id == o.video_id and
+                    self.title == o.title and
+                    self.like_count == o.like_count and
+                    self.view_count == o.view_count)
+
+        return False
+
     def __init__(self, video_id, title, like_count, view_count):
         self.video_id = video_id
         self.title = title
