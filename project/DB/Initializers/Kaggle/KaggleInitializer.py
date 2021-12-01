dd63ede7a105d3bc789f1d982ed0423fbb633846
Mykola Popeliuk <mycolapopelyuk@gmail.com>
2021-12-01 02:39:27 +0200

    modified db initializers

--- project/DB/Initializers/Kaggle/KaggleInitializer.py
+++ project/DB/Initializers/Kaggle/KaggleInitializer.py
@@ -6,6 +6,6 @@ class KaggleInitializer(AbstractInitializer):
         try:
             connection.get_connection().set_session(autocommit=True)
             with connection.get_connection().cursor() as cursor:
-                cursor.execute(open("DB/Initializers/Kaggle/generate_tables.sql", "r").read())
+                cursor.execute(open("project/DB/Initializers/Kaggle/generate_tables.sql", "r").read())
         except Exception as _ex:
             print("[ERROR] Error while creating table with PostgreSQL", _ex)
--- project/DB/Initializers/YouTube/YouTubeInitializer.py
+++ project/DB/Initializers/YouTube/YouTubeInitializer.py
@@ -6,6 +6,6 @@ class YouTubeInitializer(AbstractInitializer):
         try:
             connection.get_connection().set_session(autocommit=True)
             with connection.get_connection().cursor() as cursor:
-                cursor.execute(open("DB/Initializers/YouTube/generate_tables.sql", "r").read())
+                cursor.execute(open("project/DB/Initializers/YouTube/generate_tables.sql", "r").read())
         except Exception as _ex:
             print("[ERROR] Error while creating table with PostgreSQL", _ex)
